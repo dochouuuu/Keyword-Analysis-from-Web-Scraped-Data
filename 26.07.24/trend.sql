248c9322a6136c8bfd7d9bffb56a400e3bfb6f06
dochouuuu <dotranminhchau.259@gmail.com>
2024-07-22 14:47:00 +0700

    Create trend.sql

---
+++ 26.07.24/trend.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE TREND; 
+GO
+
+USE TREND; 
+GO
+
+DROP TABLE if exists keywords; 
+DROP TABLE if exists articles; 
+DROP TABLE if exists categories; 
+
+CREATE TABLE categories (
+    id INT PRIMARY KEY IDENTITY, 
+    name NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE articles (
+    id INT PRIMARY KEY IDENTITY,
+    category_id INT REFERENCES categories(id), 
+    timestamp DATETIMEOFFSET NOT NULL, 
+    title NVARCHAR(255) NOT NULL,
+    url NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE keywords (
+    id INT PRIMARY KEY IDENTITY,
+    article_id INT REFERENCES articles(id),
+    keyword NVARCHAR(255) NOT NULL
+);
+GO
+
+SELECT * FROM categories; 
+SELECT * FROM articles; 
+SELECT * FROM keywords; 
